58d6bde044d0364ac44fb6bdf1c3dd6d1dd86614
valente alvarez <valvarez@basecampcodingacademy.org>
2017-10-16 14:15:02 -0500

    added a new parameter to the gladiator function then created a function that will show the user the gladiator info.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,32 @@
 const $ = require('jquery');
 const other = require('../../lib/other');
 
-function gladiator(name, health, high_damage, low_damage) {
+function gladiator(name, health, rage, high_damage, low_damage) {
     var gladiator = {};
     gladiator.name = name;
     gladiator.health = health;
+    gladiator.rage = rage;
     gladiator.high_damage = high_damage;
     gladiator.low_damage = low_damage;
     return gladiator;
+}
+function gladiator(gladiator) {
+    return (
+        '<li>' +
+        gladiator.name +
+        ':' +
+        gladiator.health +
+        ' HP ||| ' +
+        gladiator.rage +
+        ' Rage ||| Low damage: ' +
+        gladiator.low_damage +
+        ' ||| High damage: ' +
+        gladiator.high_damage +
+        '</li>'
+    );
+}
+function randInt(lo, hi) {
+    return lo + Math.floor(Math.random() * (hi - lo));
 }
 
 function main() {}
