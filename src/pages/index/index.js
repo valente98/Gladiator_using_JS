bf29dcc57626e60f0ced04218d9ac83ce14b8a0f
valente alvarez <valvarez@basecampcodingacademy.org>
2017-10-16 13:58:38 -0500

    added a function that will create the gladiators.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,14 @@
 const $ = require('jquery');
 const other = require('../../lib/other');
 
+function gladiator(name, health, high_damage, low_damage) {
+    var gladiator = {};
+    gladiator.name = name;
+    gladiator.health = health;
+    gladiator.high_damage = high_damage;
+    gladiator.low_damage = low_damage;
+    return gladiator;
+}
+
 function main() {}
 $(main);
