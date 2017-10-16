edc3a7caf97fc07b73c2e1475be97b6381b659f3
valente alvarez <valvarez@basecampcodingacademy.org>
2017-10-16 13:34:41 -0500

    deleted the function

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,24 +1,5 @@
 const $ = require('jquery');
 const other = require('../../lib/other');
 
-function gladiators(info, info_2) {
-    var State = {
-        gladiator_1: {
-            name: info,
-            Health: 100,
-            rage: 0,
-            Damage_high: 20,
-            Damage_low: 12
-        },
-        gladiator_2: {
-            name: info_2,
-            Health: 100,
-            Rage: 0,
-            Damgae_high: 30,
-            Damage_low: 5
-        }
-    };
-    return State;
-}
 function main() {}
 $(main);
