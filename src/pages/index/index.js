a3cc28e85ad20e0692655d40b57f995c596f3e81
valente alvarez <valvarez@basecampcodingacademy.org>
2017-10-16 12:55:21 -0500

    deleted some function and replaced it with new function. created a function that will keep up with the gladiators info. and created function that will show the user the information about the gladiator but is not working yet.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,19 +1,53 @@
 const $ = require('jquery');
 const other = require('../../lib/other');
 
-function hello() {
-    $('body').append('<h1> Good Bye!</h1>');
-    other.foo();
+function gladiators(info, info_2) {
+    var State = {
+        gladiator_1: {
+            name: info,
+            Health: 100,
+            rage: 0,
+            Damage_high: 20,
+            Damage_low: 12
+        },
+        gladiator_2: {
+            name: info_2,
+            Health: 100,
+            Rage: 0,
+            Damgae_high: 30,
+            Damage_low: 5
+        }
+    };
+    return State;
+}
+function gladiator1Html(info) {
+    for (item in gladiators(info)){
+        for 
+        
+    }
 }
 
-function newFunc() {
-    return 2;
+function showGladiator1Info(info) {
+    const h = gladiator1Html(info);
+    $('#gladiator-one-info').html(h);
 }
-function add(a, b) {
-    return a + b;
+function addGladiator1Name() {
+    const input = $('#Gladiator-one-input');
+    input.on('input', function(event) {
+        showGladiator1Info(event.value);
+        enableButton();
+    });
 }
-
-$(hello);
-
-exports.hello = hello;
-exports.add = add;
+function showGladiator2Info(info) {
+    const h = gladiator2Html(info);
+    $('#gladiator-two-info').html(h);
+}
+function addGladiator2Name() {
+    const input = $('#Gladiator-two-input');
+    input.on('input', function(event) {
+        showGladiator2Info(event.value);
+        enableButton();
+    });
+}
+function main() {}
+$(main);
