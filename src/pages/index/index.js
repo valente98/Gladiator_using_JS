835e895667608926a78b0eb59bc988d5e7b6ccff
valente alvarez <valvarez@basecampcodingacademy.org>
2017-10-16 15:31:36 -0500

    added a function call that will create the gladiators.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -70,5 +70,8 @@ function is_dead(gladiator) {
     }
 }
 
-function main() {}
+function main() {
+    var player_1 = gladiator($('#Gladiator-one-input'), 200, 0, 20, 12);
+    var player_2 = gladiator($('#Gladiator-two-input'), 200, 0, 20, 12);
+}
 $(main);
